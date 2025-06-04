4f736502c718f91a1242f49cb4d752e0d29ecab1
Eyeruse <eyerusalemg11@gmail.com>
2025-06-04 17:33:29 -0400

    Axios updated

--- Amazon-Front-End-main/src/api/axios.js
+++ Amazon-Front-End-main/src/api/axios.js
@@ -2,6 +2,8 @@ import axios from "axios";
 const axiosInstance = axios.create({
   // baseURL: "http://127.0.0.1:5001/fir-d7510/us-central1/api",
   // baseURL: "http://localhost:5000",
+
+  // deployed version of amazon server on render.com
   baseURL: "https://amazon-backend-pzms.onrender.com",
 });
 export { axiosInstance };
